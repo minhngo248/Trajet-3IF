662c70dad1e3962729c26ef850ede7f8199af5ad
minhngo248 <minhngo248@users.noreply.github.com>
2021-11-17 17:17:35 +0100

    mess

--- ListeTrajet.cpp
+++ ListeTrajet.cpp
@@ -15,7 +15,7 @@ using namespace std;
 #include <iostream>
 #include "ListeTrajet.h"
 
-//-------------------------------------------- Constructeurs - destructeur
+//------ Constructeurs - destructeur
 ListeTrajet::ListeTrajet() {
 	head = new NodeTrajet();
 	head = NULL;	
@@ -34,37 +34,60 @@ void ListeTrajet::ajouter_en_tete(Trajet trajet) {
 	}
 }
 
-Xxx::Xxx ( const Xxx & unXxx )
-// Algorithme :
-//
+void ListeTrajet::rechercher ( Trajet trajet ) {
+	int i=0;
+	NodeTrajet* p = new NodeTrajet();
+	p = head;
+
+	while (p != NULL) {
+		if (p->trajet == trajet) {
+			printf("pos : %d\n", i);
+		}
+		p = p->next;
+		i++;
+	}
+}
+
+void ListeTrajet::ajouter_en_queue(Trajet trajet) 
 {
-#ifdef MAP
-    cout << "Appel au constructeur de copie de <Xxx>" << endl;
-#endif
-} //----- Fin de Xxx (constructeur de copie)
+	NodeTrajet* p = new NodeTrajet();
+	p = head;
+	while(p->next != NULL) {
+		p = p->next;
+	}
+	NodeTrajet* q = new NodeTrajet();
+	q->trajet = trajet;
+	q->next = NULL;
+	p->next = q;	
+}
+
+void ListeTrajet::afficher() {
+	NodeTrajet* p = head;
+	while (p != NULL) {
+	//	printf("%d\t", p->value);
+		p = p->next;
+	}
+}
+
+void ListeTrajet::supprimer(Trajet trajet) {
+	NodeTrajet* p = new NodeTrajet();
+	if (head->trajet == trajet) {
+		head = head->next;
+	}
+	p = head;
+	while(p->next != NULL) {
+		if(p->next->trajet == trajet) {
+			NodeTrajet* q = new NodeTrajet();
+			q = p->next;
+			p->next = q->next;
+			delete q;
+		}
+		p = p->next;
+	}
+}
 
 
-Xxx::Xxx ( )
-// Algorithme :
-//
+ListeTrajet::~ListeTrajet ()
 {
-#ifdef MAP
-    cout << "Appel au constructeur de <Xxx>" << endl;
-#endif
-} //----- Fin de Xxx
-
-
-Xxx::~Xxx ( )
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au destructeur de <Xxx>" << endl;
-#endif
-} //----- Fin de ~Xxx
-
-
-//------------------------------------------------------------------ PRIVE
-
-//----------------------------------------------------- Méthodes protégées
-
+	delete head;
+}
--- ListeTrajet.h
+++ ListeTrajet.h
@@ -6,151 +6,19 @@
     e-mail               : $EMAIL$
 *************************************************************************/
 
-//---------- Interface de la classe <Xxx> (fichier Xxx.h) ----------------
+//---------- Interface de la classe <listtrajet> (fichier Xxx.h) ----------------
 #if ! defined ( LISTE_TRAJET_H )
 #define LISTE_TRAJET_H
-
-//--------------------------------------------------- Interfaces utilisées
-
-//------------------------------------------------------------- Constantes
-
-//------------------------------------------------------------------ Types
-
-//------------------------------------------------------------------------
-// Rôle de la classe <Xxx>
-//
-//
-//------------------------------------------------------------------------
-
-
-
-//----------------------------------------------------- Méthodes publiques
-    // type Méthode ( liste des paramètres );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-	
-
-//------------------------------------------------- Surcharge d'opérateurs
-
-
-//-------------------------------------------- Constructeurs - destructeur
-
-
-
-//----------------------------------------------------- Méthodes protégées
 class ListeTrajet {
 	public:
 		NodeTrajet* head;
 	public:
 		ListeTrajet();
-		void ajouter_en_tete(); 
-
-		void rechercher(); {
-		int x,i=0;
-	elem* p = malloc(sizeof(elem));
-	p = list;
-	printf("Vous voulez trouver quel nombre ");
-	scanf("%d", &x);
-	while (p != NULL) {
-		if (p->value == x) {
-			printf("pos : %d\n", i);
-		}
-		p = p->next;
-		i++;
-	}
-}
-
-void ajouter_en_queue() {
-	int x;
-	printf("Entrez un nombre ");
-	scanf("%d", &x);
-	elem* p = malloc(sizeof(elem));
-	p = list;
-	while(p->next != NULL) {
-		p = p->next;
-	}
-	elem* q = malloc(sizeof(elem));
-	q->value = x;
-	q->next = NULL;
-	p->next = q;
-} 
-
-void afficher() {
-	elem* p = list;
-	while (p != NULL) {
-		printf("%d\t", p->value);
-		p = p->next;
-	}
-}
-
-void supprimer() {
-	int x;
-	printf("Entrez un nombre ");
-	scanf("%d", &x);
-	elem* p = malloc(sizeof(elem));
-	if (list->value == x) {
-		list = list->next;
-	}
-	p = list;
-	while(p->next != NULL) {
-		if(p->next->value == x) {
-			elem* q = malloc(sizeof(elem));
-			q = p->next;
-			p->next = q->next;
-			free(q);
-		}
-		p = p->next;
-	}
-}
-
-void dupliquer() {
-	int x;
-	printf("Entrez un nombre ");
-	scanf("%d", &x);
-	elem *p = malloc(sizeof(elem));
-	p = list;
-	while (p != NULL) {
-		if (p->value == x) {
-			elem* q = malloc(sizeof(elem));
-			q->value = x;
-			q->next = p->next;
-			p->next = q;
-			p = p->next;
-		}
-		p = p->next;
-	}
-}
-
-void ajouter_en_place() {
-	if (est_triee()) {
-		int x;
-		printf("Entrez x ");
-		scanf("%d", &x);
-		elem* p = malloc(sizeof(elem));
-		if(list->value > x) {
-			p->value = x;
-			p->next = list;
-			list = p;
-		}
-		else {
-			p = list;
-			while (p->next->value < x && p->next != NULL) {
-				p = p->next;
-			}
-			elem* q = malloc(sizeof(elem));
-			q->value = x;
-			q->next = p->next;
-			p->next = q;
-		}
-	}	
-}
-//----------------------------------------------------- Attributs protégés
-
+		void ajouter_en_tete(Trajet); 
+		void rechercher(Trajet);
+		void ajouter_en_queue(Trajet); 
+		void afficher(); 
+		void supprimer(Trajet);  
 };
 
-//-------------------------------- Autres définitions dépendantes de <Xxx>
-
-#endif // XXX_H
-
+#endif
