9fa692ff3bdd0f2d1120a5c3e18db895b6f975a7
QVPham9601 <vietpham961@gmail.com>
2021-12-27 22:56:39 +0100

    first 3 parts done

--- main.cpp
+++ main.cpp
@@ -17,89 +17,137 @@ using namespace std;
 #include "TrajetSimple.h"
 #include "TrajetCompose.h"
 
-void addTrajetSimple (Catalogue* C, string line) {
-	string delimiter2 = ", ";
+string toUpper (const string& str) {
+	char c;
+	unsigned i;
+	string s = "";
+	for (i=0; i < str.length(); ++i) {
+		c = str.at(i);
+		if (c >= 97 && c <= 122) {
+			c = c -32;
+		}
+		s.append(1,c);
+	}
+	return s;	
+}
+
+void addTrajetSimple (Catalogue* C, string line, TrajetCompose* TC = nullptr, const int k =0, const string VD = "", const string VA = "") {
+	bool bVD = (toUpper(VD) == "NON"); // Si l'utilisateur n'avez pas besoin de une ville de depart specifique
+	bool bVA = (toUpper(VA) == "NON"); // Si l'utilisateur n'avez pas besoin de une ville d'arrivee specifique
+	
+	string delimiter = ", ";
 	string* membre = new string[3];
 	
-	size_t posD2 = 0;
+	size_t pos = 0;
 	int i =0;
 	
 	TrajetSimple * TS;
 	
-	line.append(delimiter2);
-	while ((posD2 = line.find(delimiter2)) != string::npos) {
-		membre[i] = line.substr(0, posD2);
-		line.erase(0, posD2 + delimiter2.length());
+	line.append(delimiter);
+	while ((pos = line.find(delimiter)) != string::npos) {
+		membre[i] = line.substr(0, pos);
+		line.erase(0, pos + delimiter.length());
 		i++;
 	}
+	
+	if (k ==3) {
+		if (!((toUpper(membre[0]) == VD || bVD) && (toUpper(membre[1]) == VA || bVA))) {
+			return;
+		}
+	}
+	
 	TS = new TrajetSimple(membre[0],membre[1],membre[2]);
-	C->Ajouter_trajet(TS);
+	if (TC == nullptr) {
+		C->Ajouter_trajet(TS);
+	}else{
+		TC->Ajouter(TS);
+	}
 }
 
-void addTrajetCompose (Catalogue* C, string line) {
-	string characTrajet;
+void addTrajetCompose (Catalogue* C, string line, const int k =0, const string VD = "", const string VA = "") {
+	bool bVD = (toUpper(VD) == "NON"); // Si l'utilisateur n'avez pas besoin de une ville de depart specifique
+	bool bVA = (toUpper(VA) == "NON"); // Si l'utilisateur n'avez pas besoin de une ville d'arrivee specifique
 	
-	string delimiter1 = "; ";
-	string delimiter2 = ", ";
-	string* membre = new string[3];
+	string characTrajet;	
+	string delimiter = "; ";
 	
-	size_t posD1 = 0;	
-	size_t posD2 = 0;
-	int i;
+	size_t pos = 0;	
 	
-	TrajetSimple * TS;
 	TrajetCompose* TC = new TrajetCompose();
 	
-	line.append(delimiter1);
-	while ((posD1 = line.find(delimiter1)) != string::npos) {
-		i =0;
-		characTrajet = line.substr(0,posD1);
-		characTrajet.append(delimiter2);
-		while ((posD2 = characTrajet.find(delimiter2)) != string::npos) {
-			membre[i] = characTrajet.substr(0, posD2);
-			characTrajet.erase(0, posD2 + delimiter2.length());
-			i++;
-		}
-		TS = new TrajetSimple(membre[0],membre[1],membre[2]);
-		TC->Ajouter(TS);				
-		line.erase(0, posD1 + delimiter1.length());
+	line.append(delimiter);
+	while ((pos = line.find(delimiter)) != string::npos) {
+		characTrajet = line.substr(0,pos);
+		addTrajetSimple(C, characTrajet, TC);				
+		line.erase(0, pos + delimiter.length());
 	}
-	C->Ajouter_trajet(TC);	
+	if (k ==3) {
+		if (!((toUpper(TC->GetVille(1)) == toUpper(VD) || bVD) && (toUpper(TC->GetVille(2)) == toUpper(VA) || bVA))) {
+			delete TC;
+			return;
+		}
+	}
+	C->Ajouter_trajet(TC);
 }
 
-void addFromFile(Catalogue* C, const string fileName) {
+void addFromFile(Catalogue* C, const string& fileName, const int k, const string trajet = "", const string VD = "", const string VA = "") {
+	
 	ifstream myFile;
 	string line;
 	string typeTrajet;
 	
 	string delimiter = ": ";
+	size_t pos = 0;	
 	
-	size_t posD1 = 0;	
-	
-	TrajetSimple * TS = new TrajetSimple();
-	TrajetCompose* TC;
+	TrajetSimple * TS;
+	TrajetCompose* TC = nullptr;
 	
 	myFile.open(fileName);
 	while (getline(myFile,line)) {
-		posD1 = line.find(delimiter);
-		typeTrajet = line.substr(0, posD1);
-		line.erase(0, posD1 + delimiter.length());
+		pos = line.find(delimiter);
+		typeTrajet = line.substr(0, pos);
+		line.erase(0, pos + delimiter.length());
 		
-		if (typeTrajet.compare("TS") ==0) {
-			addTrajetSimple(C, line);
-		}
-		
-		else if (typeTrajet.compare("TC") ==0) {
-			addTrajetCompose(C, line);	
-		}
+		switch(k) {
+			case 1:
+				if (typeTrajet.compare("TS") ==0) {
+					addTrajetSimple(C, line);
+				}
+				else {
+					addTrajetCompose(C, line);	
+				}
+				break;
+			case 2:
+				if (trajet.compare("TS") ==0) {
+					if (typeTrajet.compare("TS") ==0) {
+						addTrajetSimple(C, line);
+					}
+				}else{
+					if (typeTrajet.compare("TC") ==0) {
+						addTrajetCompose(C, line);
+					}
+				}
+				break;
+			case 3:
+				if (typeTrajet.compare("TS") ==0) {
+					addTrajetSimple(C, line, TC, k, VD, VA);
+				}
+				else {
+					addTrajetCompose(C, line, k, VD, VA);	
+				}
+			case 4:
+				break;
+			default:
+				break;
+		}		
 	}
 	myFile.close();
 }
 
-void saveToFile (Catalogue* C, const string fileName, const int k, const string trajet, const string VD, const string VA) {
+void saveToFile (Catalogue* C, const string& fileName, const int k, const string trajet = "", const string VD = "", const string VA = "") {
 	
-	bool bVD = ((VD == "Non") || (VD == "non") || (VD == "NON")); // Si l'utilisateur n'avez pas besoin de une ville de depart specifique
-	bool bVA = ((VA == "Non") || (VA == "non") || (VA == "NON")); // Si l'utilisateur n'avez pas besoin de une ville d'arrivee specifique
+	bool bVD = (toUpper(VD) == "NON"); // Si l'utilisateur n'avez pas besoin de une ville de depart specifique
+	bool bVA = (toUpper(VA) == "NON"); // Si l'utilisateur n'avez pas besoin de une ville d'arrivee specifique
 	
 	ofstream myFile;
 	NodeTrajet* n;
@@ -137,14 +185,14 @@ void saveToFile (Catalogue* C, const string fileName, const int k, const string 
 			break;	
 		case 3:
 			while (n != nullptr) {
-				if ((n->GetTrajet()->GetVille(1).compare(VD)==0 || bVD) && (n->GetTrajet()->GetVille(2).compare(VA)==0 || bVA)) {
+				if ((toUpper(n->GetTrajet()->GetVille(1)) == toUpper(VD) || bVD) && ((toUpper(n->GetTrajet()->GetVille(2)) == toUpper(VA) || bVA))) {
 					n->GetTrajet()->FicWrite(myFile, 1);	
 				}
 				n = n->GetNext();
 			}
 			break;
 		default:
-			cout <<"a";
+			break;
 	}
 }
 
@@ -156,7 +204,7 @@ int main() {
 	Catalogue* C = new Catalogue();
 	TrajetCompose* unTC = new TrajetCompose();
 	TrajetSimple* t = new TrajetSimple();
-
+	
 	do {	
 		//Menu
 		cout << "Veuillez choisir une demande \r\n" << endl;
@@ -175,7 +223,51 @@ int main() {
 			
 		switch(n) {
 			case 1:
-				addFromFile(C, "Trajets.txt");
+				cout << endl << "-------CHARGEMENT D'UN CATALOGUE DE TRAJETS-----\r\n" << endl;
+				cout << "Vous voulez charger quels trajets ? " << endl ;
+				cout << "1. Chargement de tous les trajets. " << endl ;
+				cout << "2. Chargement de tous les trajets simples ou de trajets composes." << endl ;
+				cout << "3. Chargement de tous les trajets selon une ville de depart ou une ville d'arrivee." << endl ;
+				cout << "4. Chargement de (n - m + 1) trajets." << endl ;
+				cout << "5. Quitter" << endl;
+				cout << endl << "Choississez un choix" << endl ;
+				
+				cin >> c_n;
+				cin.get();
+				n = c_n -'0';
+				
+				switch(n) {
+					case 1:
+						addFromFile(C, "Trajets.txt", 1);
+						break;
+					case 2:
+						cout << "1. Chargement de tous les trajets simples" << endl;
+						cout << "2. Chargement de tous les trajets composes" << endl;
+						cout << "3. Quitter" << endl;
+						cout << "Veuillez choisir un choix" << endl;
+						cin >> k;
+						cin.get();
+						if (k == 1) {
+							addFromFile(C, "Trajets.txt", 2, "TS");
+						}else if (k==2){
+							addFromFile(C, "Trajets.txt", 2, "TC");
+						}
+						break;
+					case 3:
+						cout << "Entrez une ville du depart (Entrez Non/non si vous n'en avez pas besoin): ";
+						getline(cin, uneVilleDepart);
+						cout << "Entrez une ville d'arrivee (Entrez Non/non si vous n'en avez pas besoin): ";
+						getline(cin, uneVilleArrivee);
+						
+						addFromFile(C, "Trajets.txt", 3, "",uneVilleDepart , uneVilleArrivee);
+						break;
+					case 4:
+						break;
+					
+					default:
+						break;
+			
+				}	
 				break;
 			case 2:	
 				cout << endl << "-------AJOUTER UN TRAJET SIMPLE-----\r\n" << endl;
@@ -235,13 +327,14 @@ int main() {
 				C->Afficher();
 				break;			
 			case 7:
+				cout << endl << "-------SAUVEGARDE DU CATALOGUE COURANT-----\r\n" << endl;
 				cout << "Vous voulez sauvegarder quels trajets ? " << endl ;
 				cout << "1. Sauvegarde de tous les trajets. " << endl ;
 				cout << "2. Sauvegarde de tous les trajets simples ou de trajets composes." << endl ;
 				cout << "3. Sauvegarde de tous les trajets selon une ville de depart ou une ville d'arrivee." << endl ;
 				cout << "4. Sauvegarde de (n - m + 1) trajets." << endl ;
 				cout << "5. Quitter" << endl;
-				cout << "Choississez un choix" << endl ;
+				cout << endl << "Choississez un choix" << endl ;
 				
 				cin >> c_n;
 				cin.get();
@@ -249,7 +342,7 @@ int main() {
 				
 				switch(n) {
 					case 1:
-						saveToFile(C, "Trajets.txt", 1, "", "", "");
+						saveToFile(C, "Trajets.txt", 1);
 						break;
 					case 2:
 						cout << "1. Sauvegarde de tous les trajets simples" << endl;
@@ -259,15 +352,15 @@ int main() {
 						cin >> k;
 						cin.get();
 						if (k == 1) {
-							saveToFile(C, "Trajets.txt", 2, "TS", "", "");
+							saveToFile(C, "Trajets.txt", 2, "TS");
 						}else if (k==2){
-							saveToFile(C, "Trajets.txt", 2, "TC", "", "");
+							saveToFile(C, "Trajets.txt", 2, "TC");
 						}
 						break;
 					case 3:
-						cout << "Entrez une ville du depart (Entrez Non/non si vous n'en avez pas besoin):";
+						cout << "Entrez une ville du depart (Entrez Non/non si vous n'en avez pas besoin): ";
 						getline(cin, uneVilleDepart);
-						cout << "Entrez une ville d'arrivee (Entrez Non/non si vous n'en avez pas besoin):";
+						cout << "Entrez une ville d'arrivee (Entrez Non/non si vous n'en avez pas besoin): ";
 						getline(cin, uneVilleArrivee);
 						
 						saveToFile(C, "Trajets.txt", 3, "",uneVilleDepart , uneVilleArrivee);
@@ -290,13 +383,9 @@ int main() {
 			
 		cout << endl << "Tapez Y ou y pour continuer : " << endl;
 		cin >> c;				
-	}while(c == 'Y' || c == 'y');
+	} while(c == 'Y' || c == 'y');
 	
-
-//	delete t;
-//	delete unTC;
 	delete C;
-	
-	
+		
 	return 0;
 }
